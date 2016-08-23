71995d3c268d569a76772ce9ff23a690f3c8fb5e
Udit <uditjindal3@yahoo.com>
2016-08-23 12:27:34 +0530

    - initial commit

---
+++ src/chatRoom/ChatRoom.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatRoom;
+
+import java.util.List;
+import user.User;
+
+/**
+ *
+ * @author Raghu
+ */
+public class ChatRoom {
+    List<User> users;
+}
---
+++ src/chatserver/ChatServer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatserver;
+
+import chatRoom.ChatRoom;
+import java.util.List;
+import user.User;
+
+/**
+ *
+ * @author Raghu
+ */
+public class ChatServer {
+
+    List<ChatRoom> chatRooms;
+    List<User> users;
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
